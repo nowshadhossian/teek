9b35a72e9cc05a4a686471e58bf66a4492d1557d
nowshad <nowshad.club@gmail.com>
2019-10-20 00:03:30 +0600

    hysterix implemented

--- buyer/src/main/java/com/kids/buyer/BuyerApplication.java
+++ buyer/src/main/java/com/kids/buyer/BuyerApplication.java
@@ -3,14 +3,17 @@ package com.kids.buyer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableCaching
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableCircuitBreaker
 public class BuyerApplication {
 
     public static void main(String[] args) {
--- buyer/src/main/java/com/kids/buyer/service/BuyerSupplierService.java
+++ buyer/src/main/java/com/kids/buyer/service/BuyerSupplierService.java
@@ -1,5 +1,6 @@
 package com.kids.buyer.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
@@ -13,10 +14,15 @@ public class BuyerSupplierService {
 
     @Autowired private RestTemplate restTemplate;
 
-   /* @Cacheable("myTest")*/
+    @HystrixCommand(fallbackMethod = "defaultTest")
     public String test(){
         return restTemplate.getForObject("http://supplier-service/get", String.class);
     }
+
+    public String defaultTest(){
+        return "I am front hysterix because supplier went down";
+    }
+
 
     @CacheEvict("myTest")
     public void cacheEvict(){
